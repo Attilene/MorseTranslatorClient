d807b4ce73d2f2f537a15a2d1ab4f049a8e3041e
Artem Bakanov <artembakanov123@yandex.ru>
2021-03-02 19:51:38 +0300

    Added css-style for Alerts

--- src/sample/utils/AlertsUtil.java
+++ src/sample/utils/AlertsUtil.java
@@ -1,8 +1,11 @@
 package sample.utils;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
@@ -12,6 +15,9 @@ public abstract class AlertsUtil {
         alert.initOwner(stage);
         alert.setTitle("Удаление профиля");
         alert.setHeaderText("Вы уверены, что хотите удалить свой профиль?");
+        for (ButtonType type: alert.getButtonTypes())
+            ((Button) alert.getDialogPane().lookupButton(type)).setDefaultButton(type == ButtonType.NO);
+        addStyleSheetToAlert(alert);
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK){
             stage.close();
@@ -25,6 +31,7 @@ public abstract class AlertsUtil {
         alert.setTitle("Неправильно заполненные поля");
         alert.setHeaderText("Исправьте неправильно заполненные поля");
         alert.setContentText(message);
+        addStyleSheetToAlert(alert);
         alert.showAndWait();
     }
 
@@ -34,6 +41,7 @@ public abstract class AlertsUtil {
         alert.setTitle("Неверный формат строки");
         alert.setHeaderText("Неправильный формат текстового поля: " + name);
         alert.setContentText("Не допускаются символы: % \" ' ; :");
+        addStyleSheetToAlert(alert);
         alert.showAndWait();
     }
 
@@ -43,6 +51,7 @@ public abstract class AlertsUtil {
         alert.setTitle("Ошибка входа");
         alert.setHeaderText("Недействительные логин/почта или пароль!");
         alert.setContentText("Введите корректные данные или зарегистрируйтесь");
+        addStyleSheetToAlert(alert);
         alert.showAndWait();
     }
 
@@ -51,6 +60,7 @@ public abstract class AlertsUtil {
         alert.initOwner(stage);
         alert.setTitle("Неправильный формат почты");
         alert.setHeaderText("Введите корректную электронную почту!");
+        addStyleSheetToAlert(alert);
         alert.showAndWait();
     }
 
@@ -64,6 +74,7 @@ public abstract class AlertsUtil {
                 Пароль должен содержать:
                 заглавные буквы, строчные буквы, цифры,
                 специальные символы из списка: @ # $ %""");
+        addStyleSheetToAlert(alert);
         alert.showAndWait();
     }
 
@@ -73,6 +84,7 @@ public abstract class AlertsUtil {
         alert.setTitle("Неправильный формат номера телефона");
         alert.setHeaderText("Введите номер телефона в нужном формате");
         alert.setContentText("Формат: +7{номер телефона} или 8{номер телефона}");
+        addStyleSheetToAlert(alert);
         alert.showAndWait();
     }
 
@@ -82,6 +94,7 @@ public abstract class AlertsUtil {
         alert.setTitle("Превышение длины строки");
         alert.setHeaderText("Длина поля " + name + " больше " + symbols + " символов!");
         alert.setContentText("Измените значение текстового поля");
+        addStyleSheetToAlert(alert);
         alert.showAndWait();
     }
 
@@ -91,6 +104,7 @@ public abstract class AlertsUtil {
         alert.setTitle("Пользователь существует");
         alert.setHeaderText("Пользователь с такими данными уже существует!");
         alert.setContentText("Логин, почта или номер телефона должны быть уникальными");
+        addStyleSheetToAlert(alert);
         alert.showAndWait();
     }
 
@@ -100,6 +114,14 @@ public abstract class AlertsUtil {
         alert.setTitle("Ошибка сервера");
         alert.setHeaderText("Сервер временно недоступен!");
         alert.setContentText("Свяжитесь с администратором или повторите попытку позже");
+        addStyleSheetToAlert(alert);
         alert.showAndWait();
+    }
+
+    public static void addStyleSheetToAlert(@NotNull Alert alert) {
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.getStylesheets().add(
+                AlertsUtil.class.getResource("../resources/styles/DialogStyle.css").toExternalForm());
+        dialogPane.getStyleClass().add("dialog-pane");
     }
 }
